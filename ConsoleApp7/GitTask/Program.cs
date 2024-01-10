93a73c1c33f5bb052b93f0c9eec970bbcde2a5f9
serjo132 <56090335+serjo132@users.noreply.github.com>
2024-01-10 19:04:32 +0300

    Update Program.cs

--- ConsoleApp7/GitTask/Program.cs
+++ ConsoleApp7/GitTask/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+namespace HelloWorld
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
